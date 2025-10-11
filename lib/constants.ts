8469fc728641fb0b5fa08a3f1adccc5bccb1f04e
Prathmesh Sarda <diamond123erock@gmail.com>
2025-10-12 01:32:25 +0530

    implemented header

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const NAV_ITEMS = [
+    {
+        href:'/',label:'DashBoard'
+    },
+    {
+        href:'/search',label:'Search'
+    },
+    {
+        href:'/watchlist',label:'watchlist'
+    },
+]
